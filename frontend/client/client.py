79eaa928d4dbf83bfbe5f3b93c112b4cfb68e5dd
Vlad Bogush <bogush22092209@gmail.com>
2023-04-11 11:48:01 +0200

    q

---
+++ frontend/client/client.py
@@ -0,0 +1,13 @@
+# echo-client.py
+
+import socket
+
+HOST = "192.168.0.111"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hi")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
