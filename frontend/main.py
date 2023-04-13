a5905c47685be2b76cafe073be881d4dd06f6c14
Vlad Bogush <bogush22092209@gmail.com>
2023-04-13 21:25:06 +0200

    q

--- frontend/main.py
+++ frontend/main.py
@@ -1,22 +1,9 @@
-from encrypt.asymmetric import AsymmCipher
-from encrypt.symmetric import SymmCipher
+from client.client import Client
 
-
-def reciev(encryptedPacket, my_private_key):
-    cp, cpSymmKey = encryptedPacket
-    
 
 def main():
-    asymCipher = AsymmCipher()
-    asymCipher.gen_private_public_key()
-    symCipher = SymmCipher()
-    symCipher.gen_key_iv()
-    message = b'hi Vlad'
-    cp = symCipher.encrypt(message)
-    cpSymmKey = asymCipher.encrypt(symCipher.key)
-    encryptedPacket = (cp, cpSymmKey)
-    reciev(encryptedPacket, asymCipher.private_key)
-    
+    client  = Client()
+
 
 
 if __name__ == '__main__':
